6e4017c20ce7e18ed8155c37fc293989647d60c3
kyleaure <kylejaure@gmail.com>
2021-01-29 15:41:03 -0600

    create full fat sub categories

--- .github/workflow-scripts/run-fat-build.sh
+++ .github/workflow-scripts/run-fat-build.sh
@@ -11,28 +11,18 @@ FAT_ARGS="-Dfattest.timeout=2700000"
 
 # If this is the special 'MODIFIED_*_MODE' job, figure out which buckets 
 # were directly modfied (if any) so they can be launched
-if [[ $CATEGORY =~ MODIFIED_.*_MODE ]]; then
-  if [[ $CATEGORY == 'MODIFIED_FULL_MODE' ]]; then
+if [[ $CATEGORY =~ MODIFIED_FULL_MODE* ]]; then
     echo "FAT buckets in this job will run in FULL mode"
     # Give FULL mode buckets a 2h timeout each
     FAT_ARGS="-Dfat.test.mode=FULL -Dfattest.timeout=7200000"
-  fi
-  git diff --name-only HEAD^...HEAD^2 >> modified_files.diff
-  echo "Modified files are:"
-  cat modified_files.diff
-  FAT_BUCKETS=$(sed -n "s/^dev\/\([^\/]*_fat[^\/]*\)\/.*$/\1/p" modified_files.diff | uniq)
-  if [[ -z $FAT_BUCKETS ]]; then
-    echo "No FATs were directly modfied. Skipping this job."
-    exit 0
-  fi
-else
-  # This is a regular category defined in .github/test-categories/
-  if [[ ! -f .github/test-categories/$CATEGORY ]]; then
-    echo "::error::Test category [$CATEGORY] does not exist. A file containing a list of FAT buckets was not found at .github/test-categories/$CATEGORY";
-    exit 1;
-  fi
-  FAT_BUCKETS=$(awk '!/^ *#/ && NF' .github/test-categories/$CATEGORY) #ignore comments starting with # and empty lines
+fi 
+
+# This is a regular category defined in .github/test-categories/
+if [[ ! -f .github/test-categories/$CATEGORY ]]; then
+  echo "::error::Test category [$CATEGORY] does not exist. A file containing a list of FAT buckets was not found at .github/test-categories/$CATEGORY";
+  exit 1;
 fi
+FAT_BUCKETS=$(awk '!/^ *#/ && NF' .github/test-categories/$CATEGORY) #ignore comments starting with # and empty lines
 
 # Ensure all buckets we plan to run exist
 echo "::group:: Will be running buckets"
