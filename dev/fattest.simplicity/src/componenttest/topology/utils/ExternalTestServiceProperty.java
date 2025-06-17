4d196028f9f4c56a0e9228181f3b2c616ea2822a
Kyle Aure <KyleJAure@gmail.com>
2025-06-17 11:18:03 -0500

    refactor: move ServiceProperty into it's own class

---
+++ dev/fattest.simplicity/src/componenttest/topology/utils/ExternalTestServiceProperty.java
@@ -0,0 +1,75 @@
+/*******************************************************************************
+ * Copyright (c) 2025 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+package componenttest.topology.utils;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.CodingErrorAction;
+import java.util.Base64;
+import java.util.Objects;
+
+/**
+ * This is a map of key value pairs that represent an external test service.
+ * Includes additional methods to decode and decrypt values.
+ *
+ * Key - holds service property key
+ * Value - holds the raw value as it was returned from consul
+ */
+class ExternalTestServiceProperty {
+
+    private final String key;
+    private final String rawValue;
+
+    private String decodedValue = null;
+    private String decryptedValue = null;
+
+    ExternalTestServiceProperty(String key, String rawValue) {
+        this.key = Objects.requireNonNull(key);
+        this.rawValue = Objects.requireNonNull(rawValue);
+    }
+
+    String getKey() {
+        return key;
+    }
+
+    String getRawValue() {
+        return rawValue;
+    }
+
+    String getDecodedValue() {
+        if (Objects.nonNull(decodedValue)) {
+            return decodedValue;
+        }
+
+        try {
+            return decodedValue = Charset.forName("UTF-8")
+                            .newDecoder()
+                            .onMalformedInput(CodingErrorAction.REPORT)
+                            .onUnmappableCharacter(CodingErrorAction.REPORT)
+                            .decode(ByteBuffer.wrap(Base64.getDecoder().decode(rawValue)))
+                            .toString();
+        } catch (CharacterCodingException e) {
+            throw new RuntimeException("Could not decode value", e);
+        }
+    }
+
+    String getDecryptedValue() {
+        if (Objects.nonNull(decryptedValue)) {
+            return decryptedValue;
+        }
+
+        try {
+            return decryptedValue = ExternalTestServiceDecrypter.decrypt(decodedValue);
+        } catch (Exception e) {
+            throw new RuntimeException("Could not decrypt value", e);
+        }
+    }
+}
